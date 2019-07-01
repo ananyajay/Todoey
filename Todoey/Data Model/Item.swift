394e07f60e0004e089f65f07fd0787dc5c39e20a
Anu Jay <anu.sk.jay@gmail.com>
2019-07-01 11:31:26 -0400

    Convert to MVC design pattern.

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  File.swift
+//  Todoey
+//
+//  Created by Ananya Jayamoorthy on 7/1/19.
+//  Copyright © 2019 Ananya Jayamoorthy. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title : String = ""
+    var done : Bool = false
+}
